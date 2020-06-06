ff17bc9e7d1ba58ecdefa1655c6aa039a669ff85
DangoSky <1341514988@qq.com>
2020-06-06 12:25:14 +0800

    feat: finish logic
    Change-Id: I03b524d46c33b5d772f188dd906c7d13677c01a3

--- parse-grammar/index.js
+++ parse-grammar/index.js
@@ -36,12 +36,14 @@ function formatGrammar(grammar) {
   const ans = [''];   // ans[0] 默认为空文法，使第 i 条文法直接对应为 ans[i]
   grammar.forEach(line => {
     const [value, expStr] = line.split('->').map(item => item.trim());
-    const exp = expStr.split('|').map(item => item.trim()); 
-    ans.push({
-      value,
-      exp
-    })
+    expStr.split('|').map(item => {
+      ans.push({
+        value,
+        exp: item.trim()
+      })
+    }) 
   });
+  
   return ans;
 }
 
@@ -57,6 +59,12 @@ function analyze(str, table, grammar) {
   while(1) {
     const topStatus = statusStack[statusStack.length - 1];
     const cur = inputArr[0];
+    if (!isDef(table[cur][topStatus])) {
+      descArr.push('在分析表中找不到对应操作，分析失败');
+      print(statusStack, charStack, inputArr, descArr);
+      print(statusStack, charStack, inputArr, descArr);
+      return true;
+    }
     const { action, target } = table[cur][topStatus];
     print(statusStack, charStack, inputArr, descArr);
     const res = judgeAction(action, target, table, grammar, statusStack, charStack, inputArr, descArr, false);
@@ -68,7 +76,7 @@ function analyze(str, table, grammar) {
 
 function judgeAction(action, target, table, grammar, statusStack, charStack, inputArr, descArr, isAfterOpeR) {
   if (!isDef(action) || !isDef(target)) {
-    descArr.push('分析失败');
+    descArr.push('在分析表中找不到对应操作，分析失败');    
     print(statusStack, charStack, inputArr, descArr);
     return true;
   }
@@ -76,19 +84,13 @@ function judgeAction(action, target, table, grammar, statusStack, charStack, inp
     // r 操作，按文法规约符号栈 && 弹出对应的状态栈 && 规约后的值结合当前状态再次查表
     case 'r': {
       const {value, exp} = grammar[target];
-      let i, start, sum;
-      for (i=0; i<exp.length; i++) {
-        start = charStack.join('').lastIndexOf(exp[i]);
-        if (start !== -1) {
-          sum = charStack.length - start;
-          charStack.splice(start, sum, value);
-          break;
-        }
-      }
+      const start = charStack.join('').lastIndexOf(exp);
+      const sum = charStack.length - start;
+      charStack.splice(start, sum, value);
       statusStack.splice(start, sum);
       const topStatus = statusStack[statusStack.length - 1];
       const { action: action1, target: target1 } = table[value][topStatus];
-      descArr.push(`${action}${target}，使用文法 ${value}->${exp[i]} 进行规约`);
+      descArr.push(`${action}${target}，使用文法 ${value}->${exp} 进行规约`);
       judgeAction(action1, target1, table, grammar, statusStack, charStack, inputArr, descArr, true)
       break;
     }
@@ -127,18 +129,20 @@ function printOuter() {
   }
 }
 
-const print = printOuter();
+let print = printOuter();
 
 (function main() {
   const contentArr = getFileContentArr(path.resolve(root, './rules.txt'));
   const analysisTable = formatAnalysisTable(contentArr);
-  console.log(analysisTable);
   const grammarContentArr = getFileContentArr(path.resolve(root, './grammar.txt'));
   const grammarArr = formatGrammar(grammarContentArr);
   const tesContenttArr = getFileContentArr(path.resolve(root, './test/demo.txt'));
   console.log('\x1B[32m%s\x1B[0m', `${grammarContentArr.join('\n')}\n使用上述文法对以下输入样例进行校验`);
   tesContenttArr.forEach(item => {
-    console.log('\x1B[31m%s\x1B[0m', `\n${item}`);
-    analyze(item, analysisTable, grammarArr);
+    if (item) {
+      console.log('\x1B[31m%s\x1B[0m', `\n${item}`);
+      print = printOuter();
+      analyze(item, analysisTable, gramgmarArr);
+    }
   })
 })()
